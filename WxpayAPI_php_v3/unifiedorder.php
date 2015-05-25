6ee0ca9dd93e97984908a97c0d0eec7de26c47a4
dong <ddliuhb@gmail.com>
2015-05-25 08:23:30 +0800

    add files

---
+++ WxpayAPI_php_v3/unifiedorder.php
@@ -0,0 +1,83 @@
+<?php 
+ini_set('date.timezone','Asia/Shanghai');
+//error_reporting(E_ERROR);
+require_once "lib/WxPay.Api.php";
+require_once "unit/WxPay.JsApiPay.php";
+require_once 'unit/log.php';
+
+//初始化日志
+$logHandler= new CLogFileHandler("./logs/".date('Y-m-d').'.log');
+$log = Log::Init($logHandler, 15);
+
+function printf_info($data)
+{
+    foreach($data as $key=>$value){
+        echo "<font color='#00ff55;'>$key</font> : $value <br/>";
+    }
+}
+
+//获取用户openid
+$tools = new JsApiPay();
+$openId = $tools->GetOpenid();
+//统一下单
+$input = new WxPayUnifiedOrder();
+$input->SetBody("test");
+$input->SetAttach("test");
+$input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
+$input->SetTotal_fee("1");
+$input->SetTime_start(date("YmdHis"));
+$input->SetTime_expire(date("YmdHis", time() + 600));
+$input->SetGoods_tag("test");
+$input->SetNotify_url("http://paysdk.weixin.qq.com/notify.php");
+$input->SetTrade_type("JSAPI");
+$input->SetOpenid($openId);
+$order = WxPayApi::unifiedOrder($input);
+echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
+printf_info($order);
+$jsApiParameters = $tools->GetJsApiParameters($order);
+echo $jsApiParameters;
+?>
+
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/> 
+    <title>微信支付样例-支付</title>
+    <script type="text/javascript">
+
+		//调用微信JS api 支付
+		function jsApiCall()
+		{
+			WeixinJSBridge.invoke(
+				'getBrandWCPayRequest',
+				<?php echo $jsApiParameters; ?>,
+				function(res){
+					WeixinJSBridge.log(res.err_msg);
+					alert(res.err_code+res.err_desc+res.err_msg);
+				}
+			);
+		}
+
+		function callpay()
+		{
+			if (typeof WeixinJSBridge == "undefined"){
+			    if( document.addEventListener ){
+			        document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
+			    }else if (document.attachEvent){
+			        document.attachEvent('WeixinJSBridgeReady', jsApiCall); 
+			        document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
+			    }
+			}else{
+			    jsApiCall();
+			}
+		}
+	</script>
+</head>
+<body>
+    <br/>
+    <font color="#9ACD32"><b>该笔订单支付金额为<span style="color:#f00;font-size:50px">1分</span>钱</b></font><br/><br/>
+	<div align="center">
+		<button style="width:210px; height:50px; border-radius: 15px;background-color:#FE6714; border:0px #FE6714 solid; cursor: pointer;  color:white;  font-size:16px;" type="button" onclick="callpay()" >立即支付</button>
+	</div>
+</body>
+</html>
